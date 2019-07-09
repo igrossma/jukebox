22df059a76509573c7a6516de517728530a4cd8d
Ingo Grossmann <ingogrossmann@icloud.com>
2019-07-09 11:50:22 +0100

    Route

--- routes/index.js
+++ routes/index.js
@@ -13,12 +13,12 @@ router.get("/playlists", (req, res, next) => {
   res.render("playlists");
 });
 
-// Create Playlist
+// Get Create Playlist Page
 router.get("/create-playlist", (req, res, next) => {
   res.render("create-playlist");
 });
 
-
+// Create Playlist in Spotify-Account
 router.post("/create-playlist", setSpotifyApi, (req, res, next) => {
   let name = req.body.playlistName;
 
@@ -35,29 +35,23 @@ router.post("/create-playlist", setSpotifyApi, (req, res, next) => {
 
 });
 
+// Get Add Playlist Page
 router.get("/add-playlist", (req, res, next) => {
-  res.render("add-playlist");
-});
 
-router.post("/add-playlist", setSpotifyApi, (req, res, next) => {
-  let search = req.body.playlistName;
 
   res.spotifyApi.getUserPlaylists(req.user.spotifyId)
     .then(function(data) {
-    console.log("This is what i type in the search bar", search)
     console.log('Retrieved playlists', data.body.items);
-  },function(err) {
+
+    res.render("add-playlists", {
+      playlist: data.body.items,
+      
+    })
+  }).catch(function(err) {
     console.log('Something went wrong!', err);
-  })
-
-  Playlist.create()
-
-  // take the input of the search form and ask spotify if the playlist exist or not
-  // if yes - show the playlist and a button add and then
-  //redirect to playlist
-
-  res.redirect("playlists");
+  }); 
 });
+
 
 router.get("/playlist-details/:playlist_id", (req, res, next) => {
   let playlistID = req.params.playlist_id;
