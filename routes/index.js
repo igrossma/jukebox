95948fdabff32aad16bbb3765e5cde4f1206090d
Ingo Grossmann <ingogrossmann@icloud.com>
2019-07-12 09:36:08 +0100

    before catch in middleware

--- routes/index.js
+++ routes/index.js
@@ -118,15 +118,15 @@ router.get("/add-playlist/:playlist_id", setSpotifyApi, (req, res, next) => {
 router.get("/playlist-details/:playlist_id",setSpotifyApi, (req, res, next) => {
     let playlistID = req.params.playlist_id;
 
-    // Push playlist to spotify every 20 seconds
-       setInterval(() => {
+    // // Push playlist to spotify every 20 seconds
+    //    setInterval(() => {
          
-         pushPlaylistToSpotify(playlistID, res.spotifyApi)
-           .then(() => {
-             res.redirect("/playlist-details/" + playlistID);
-           })
-           .catch(err => next(err));
-       }, 20000);
+    //      pushPlaylistToSpotify(playlistID, res.spotifyApi)
+    //        .then(() => {
+    //          res.redirect("/playlist-details/" + playlistID);
+    //        })
+    //        .catch(err => next(err));
+    //    }, 20000);
        
     Playlist.findById(playlistID)
       .populate("_owner")
