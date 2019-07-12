6fa27cd82c1e4cc5afc511dcf7354135cf6ed56b
Ingo Grossmann <ingogrossmann@icloud.com>
2019-07-12 12:27:11 +0100

    heroku - no refreshing but fixing sorting playlist after push

--- routes/index.js
+++ routes/index.js
@@ -123,15 +123,15 @@ router.get("/add-playlist/:playlist_id", setSpotifyApi, (req, res, next) => {
 router.get("/playlist-details/:playlist_id",setSpotifyApi, (req, res, next) => {
     let playlistID = req.params.playlist_id;
 
-    // Push playlist to spotify every 30 seconds
-       setInterval(() => {
+    // // Push playlist to spotify every 30 seconds
+    //    setInterval(() => {
          
-         pushPlaylistToSpotify(playlistID, res.spotifyApi)
-           .then(() => {
-             res.redirect("/playlist-details/" + playlistID);
-           })
-           .catch(err => next(err));
-       }, 30000);
+    //      pushPlaylistToSpotify(playlistID, res.spotifyApi)
+    //        .then(() => {
+    //          res.redirect("/playlist-details/" + playlistID);
+    //        })
+    //        .catch(err => next(err));
+    //    }, 30000);
        
     Playlist.findById(playlistID)
       .populate("_owner")
